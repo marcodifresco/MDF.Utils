b48a9b0e398e9df5e6877ecd528fe025e4dcf81b
marco <marco@PC-MARCO-WINVM>
2022-01-26 17:00:06 +0100

    Add project files.

---
+++ MDF.Utils.Logger/MDF.Utils.Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace MDF.Utils.Logger
+{
+    public class MDFUtilsLogger : MDFUtils
+    {
+        private bool toConsole = false;
+        private bool toFile = false;
+        private StreamWriter logFileName;
+        MDFUtilsLogger() { }
+        ~MDFUtilsLogger() { }
+
+        MDFUtilsLogger(string logFileToUse)
+        {
+            SetlogFileName(logFileToUse);
+        }
+
+        void SetlogFileName(string logFileToUse)
+        {
+            if (Path.GetDirectoryName(logFileToUse).Length == 0)
+            {
+                Console.WriteLine("");
+            }
+        }
+    }
+}
