ef56baa460091bc8b7f6b9bcb0a078f530c5ba19
Marco Di Fresco <marco.difresco@gmail.com>
2022-01-26 18:23:10 +0100

    Preliminary version of SetUserDataFolder.
    Signed-off-by: Marco Di Fresco <marco.difresco@gmail.com>

--- MDF.Utils.System/MDF.Utils.System.cs
+++ MDF.Utils.System/MDF.Utils.System.cs
@@ -4,8 +4,31 @@ namespace MDF.Utils.System
 {
     public class MDFUtilsSystem : MDFUtils
     {
+        private string userDataFolder = "";
         public MDFUtilsSystem()
         {
+        }
+
+        public void SetUserDataFolder(string progName)
+        {
+            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+            {
+                userDataFolder = Environment.SpecialFolder.ApplicationData + progName;
+            }
+            else
+            {
+                userDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)+ ".config/" + progName;
+            }
+        }
+
+        public string GetUserDataFolder()
+        {   // If the userDataFolder is empty, associate it with the base user folder
+            if (userDataFolder == "")
+            {
+                SetUserDataFolder("");
+            }
+
+            return userDataFolder;
         }
     }
 }
