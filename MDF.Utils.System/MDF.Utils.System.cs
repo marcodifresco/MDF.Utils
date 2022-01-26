60599408aeaaafaedc1f6243a924c665ad31d0a5
Marco Di Fresco <marco.difresco@gmail.com>
2022-01-26 22:27:04 +0100

    Added comments.
    Signed-off-by: Marco Di Fresco <marco.difresco@gmail.com>

--- MDF.Utils.System/MDF.Utils.System.cs
+++ MDF.Utils.System/MDF.Utils.System.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿/*
+ *  Provides integration with operative system functionalities 
+ */
+using System;
 using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
 
 namespace MDF.Utils.System
 {
     public class MDFUtilsSystem : MDFUtils
     {
         private string userDataFolder = "";
-        public MDFUtilsSystem()
-        {
-        }
+        public MDFUtilsSystem() {}
 
+        // Set the data folder on the os user folder (ie /home/{user}/.config on Linux or C:\Users\{user} on Windows) 
         public void SetUserDataFolder(string progName)
         {   // Get the base folder path
             if (Environment.OSVersion.Platform == PlatformID.Win32NT)
@@ -28,6 +32,7 @@ namespace MDF.Utils.System
             }
         }
 
+        // Get the data folder on the OS user folder
         public string GetUserDataFolder()
         {   // If the userDataFolder is empty, associate it with the base user folder
             if (userDataFolder == "")
@@ -36,6 +41,25 @@ namespace MDF.Utils.System
             }
 
             return userDataFolder;
+        }
+
+        public char CopyrightSymbol()
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                return "\xc2\xa9";
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                return '\xa9';
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                return '\xa9';
+            }
+
+            // Generic return
+            return '\xa9';
         }
     }
 }
